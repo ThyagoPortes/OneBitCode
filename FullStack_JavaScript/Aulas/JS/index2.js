4c80b7ba9df4f82d338e3d306940e6f5e876a162
Thyago Portes <thyagosantosoliveira@gmail.com>
2024-09-25 13:03:06 -0300

    exerciioJS

---
+++ FullStack_JavaScript/Aulas/JS/index2.js
@@ -0,0 +1,9 @@
+const calculo_1 = prompt("informe o primeiro número ")
+const calculo_2 = prompt("informe o segundo número")
+
+soma = calculo_1 + calculo_2
+sub = calculo_1 - calculo_2
+multi = calculo_1 * calculo_2
+div = calculo_1 / calculo_2
+
+alert( "\n soma:",soma + "\n subtração:",sub )
