fceefd1906cef590f4d15260f2b9df167c9ad657
Thyago Portes <thyagosantosoliveira@gmail.com>
2024-09-25 12:47:36 -0300

    aulaJS

---
+++ FullStack_JavaScript/Aulas/JS/exercicio1/index.js
@@ -0,0 +1,11 @@
+let nome = window.prompt('nome')
+let sobrenome = window.prompt('sobrenome')
+let curso = window.prompt('curso')
+let ano_nascimento = window.prompt('ano_nascimento')
+
+ano_nascimento = 2024 -(parseFloat(ano_nascimento))
+
+console.log('nome =',nome)
+console.log('sobrenome =',sobrenome)
+console.log('curso =',curso)
+console.log('idade: =',ano_nascimento)
