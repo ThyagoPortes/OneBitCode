f07f6bae0779b29056d18bf5c95e0350f476ec4b
Thyago Portes <thyagosantosoliveira@gmail.com>
2024-10-02 03:32:22 +0000

    git

---
+++ FullStack_JavaScript/Aulas/JS/visitando_novas_cidades/visitando_novas_cidades.js
@@ -0,0 +1,9 @@
+const velocidade = 80
+
+while (velocidade > 0) {
+    alert('a velocidade atual é: '+ velocidade +'km')
+    velocidade -=20
+    alert('reduzindo 20km de velocidade')
+}
+
+alert('O carro parou')
